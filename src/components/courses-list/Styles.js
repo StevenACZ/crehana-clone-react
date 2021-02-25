c344ea0d594fc6d08201f78b66bccd569219c103
stevenacz <scoailazaa@gmail.com>
2021-02-25 15:39:44 -0500

    improved

---
+++ src/components/courses-list/Styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledCoursesList = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+  grid-column-gap: 24px;
+  grid-row-gap: 40px;
+`;
