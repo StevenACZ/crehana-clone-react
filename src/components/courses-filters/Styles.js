1d8fbcf6ff0750337ce6dab9fb73c93e28bbbcc6
stevenacz <scoailazaa@gmail.com>
2021-02-25 14:27:49 -0500

    styles added

---
+++ src/components/courses-filters/Styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledCoursesFilters = styled.div`
+  display: grid;
+  align-items: flex-end;
+  border: 1px solid red;
+  gap: 24px;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+export const StyledCoursesFiltersItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  span {
+    font-size: 12px;
+    font-weight: 800;
+    line-height: 20px;
+    text-transform: uppercase;
+  }
+`;
