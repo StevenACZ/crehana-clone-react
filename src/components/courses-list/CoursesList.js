541740ec72f2a32f43fcf8a476e508246a6072b3
stevenacz <scoailazaa@gmail.com>
2021-02-25 15:38:53 -0500

    added

---
+++ src/components/courses-list/CoursesList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { StyledCoursesList } from './Styles';
+
+import { CoursesListItem } from '../courses-list-item/CoursesListItem';
+
+export const CoursesList = () => {
+  return (
+    <StyledCoursesList>
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+      <CoursesListItem />
+    </StyledCoursesList>
+  )
+}
