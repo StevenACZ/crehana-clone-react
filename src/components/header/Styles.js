2fd736e7eaf7f50ff5c51479eabeb7cf60f69e1f
stevenacz <scoailazaa@gmail.com>
2021-02-26 13:21:39 -0500

    @media query added

--- src/components/header/Styles.js
+++ src/components/header/Styles.js
@@ -1,19 +1,34 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-  width: 100%;
+  max-width: 1152px;
   height: 72px;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+  margin: 0 auto;
 
-export const HeaderContainer = styled.header`
-  width: 1152px;
-  height: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  
+  & > span {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    padding: 16px 12px;
+    height: 56px;
+
+    & > button {
+      display: none;
+    }
+
+    & > span {
+      font-size: 24px;
+      display: block;
+    }
+  }
 `;
 
 export const Logo = styled.img`
