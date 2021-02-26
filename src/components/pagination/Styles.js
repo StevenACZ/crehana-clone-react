796c485debfcd15dcfe1c4f43ff7b9c85794ee34
stevenacz <scoailazaa@gmail.com>
2021-02-26 15:54:02 -0500

    mobile and desktop pagination styles  added

--- src/components/pagination/Styles.js
+++ src/components/pagination/Styles.js
@@ -1,6 +1,52 @@
 import styled from 'styled-components';
 
 export const StyledPagination = styled.div`
+  & > div:first-child {
+    display: none;
+  }
+
+  & > div:last-child {
+    display: flex;
+  }
+
+  @media (max-width: 768px) {
+    & > div:first-child {
+      display: flex;
+    }
+
+    & > div:last-child {
+      display: none;
+    }
+  }
+`;
+
+export const MobilePagination = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 24px;
+
+  & > button {
+    border: none;
+    font-size: 32px;
+  }
+
+  & > div {
+    display: flex;
+    gap: 5px;
+
+    span {
+      font-size: 16px;
+      margin: 0;
+    }
+
+    span:first-child {
+      font-weight: 800;
+    }
+  }
+`;
+
+export const DesktopPagination = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: center;
