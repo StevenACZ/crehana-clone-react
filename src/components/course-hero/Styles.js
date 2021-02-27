fbb17b7d0293c78fc645e17e181a6b83e3110c60
stevenacz <scoailazaa@gmail.com>
2021-02-26 21:05:29 -0500

    Init styles

--- src/components/course-hero/Styles.js
+++ src/components/course-hero/Styles.js
@@ -9,3 +9,12 @@ export const MobileCourseHero = styled.section`
   width: 100%;
   height: 100%;
 `;
+
+export const HeroInformation = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+export const HeroCard = styled.div`
+  width: 100%;
+  height: 100%;
+`;
