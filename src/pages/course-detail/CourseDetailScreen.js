6784312f6d4279da6c3f27f8633f40a3419d2a60
stevenacz <scoailazaa@gmail.com>
2021-02-26 19:00:20 -0500

    New page init

--- src/pages/course-detail/CourseDetailScreen.js
+++ src/pages/course-detail/CourseDetailScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+// IsMobile?
+import {
+  isMobile
+} from "react-device-detect";
+
+// Styles
+import {
+  CourseDetail,
+} from './Styles';
+
+// Components
+import { CustomButton } from '../../components/button/CustomButton';
+
 export const CourseDetailScreen = () => {
   return (
-    <div>
+    <CourseDetail>
       
-    </div>
+    </CourseDetail>
   )
 }
---
+++ src/pages/course-detail/Styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CourseDetail = styled.main`
+  max-width: 1152px;
+  height: 100%;
+  
+  @media (max-width: 1152px) {
+  }
+
+  @media (max-width: 768px) {
+  }
+`;
