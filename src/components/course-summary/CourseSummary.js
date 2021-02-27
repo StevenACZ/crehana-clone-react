2bc210ec93a432ceeedf729420a6894d01852fb8
stevenacz <scoailazaa@gmail.com>
2021-02-27 18:40:46 -0500

    Inital component

---
+++ src/components/course-summary/CourseSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Styles
+import {
+  StyledCourseSummary
+} from './Styles';
+
+export const CourseSummary = () => {
+  return (
+    <StyledCourseSummary>
+      
+    </StyledCourseSummary>
+  )
+}
