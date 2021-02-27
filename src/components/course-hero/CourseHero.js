ad2fa7636ed85066e369732048e2dbaebeaa7604
stevenacz <scoailazaa@gmail.com>
2021-02-26 21:05:17 -0500

    Improved

--- src/components/course-hero/CourseHero.js
+++ src/components/course-hero/CourseHero.js
@@ -4,13 +4,16 @@ import React from 'react';
 import {
   BrowserView,
   MobileView,
-  isMobile
+  // isMobile
 } from "react-device-detect";
+import { GeneralCourseData } from '../general-course-data/GeneralCourseData';
 
 // Styles
 import {
   DesktopCourseHero,
-  MobileCourseHero
+  MobileCourseHero,
+  HeroInformation,
+  HeroCard
 } from './Styles';
 
 export const CourseHero = () => {
@@ -18,7 +21,25 @@ export const CourseHero = () => {
     <>
       <BrowserView>
         <DesktopCourseHero>
-          Hello Desktop
+          <HeroInformation>
+            <h2>Ttile H2 - Nombre del curso</h2>
+            <p>
+              Body 3- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id mi, mauris aliquam phasellus quis semper diam fringilla. Nunc nullam est in non. Enim sapien amet ut pharetra. Purus sagittis est felis sapien in. Auctor tellus, in mauris mi, facilisi arcu sedut.
+            </p>
+
+            <GeneralCourseData
+              level="Avanzado"
+              users="123"
+              course_score="4.8"
+            />
+
+            {/* <CourseOptions /> */}
+
+          </HeroInformation>
+
+          <HeroCard>
+
+          </HeroCard>
         </DesktopCourseHero>
       </BrowserView>
 
