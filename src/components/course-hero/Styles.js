1efe7fdc177d1e4e026990f8548aaefc9d1c1751
stevenacz <scoailazaa@gmail.com>
2021-02-26 19:26:53 -0500

    init styled

---
+++ src/components/course-hero/Styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const DesktopCourseHero = styled.section`
+  width: 100%;
+  height: 100%;
+`;
+
+export const MobileCourseHero = styled.section`
+  width: 100%;
+  height: 100%;
+`;
