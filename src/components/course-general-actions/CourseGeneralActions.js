04f125d8b9955b902f1a6e8033c5caa396276ae0
stevenacz <scoailazaa@gmail.com>
2021-02-26 23:11:17 -0500

    improved

--- src/components/course-general-actions/CourseGeneralActions.js
+++ src/components/course-general-actions/CourseGeneralActions.js
@@ -2,13 +2,34 @@ import React from 'react';
 
 // Styles
 import {
-  StyledCourseGeneralActions
+  StyledCourseGeneralActions,
+  Action
 } from './Styles';
+
+// Icons ANTD
+import {
+  HeartFilled,
+  MinusSquareFilled,
+  CameraFilled
+} from '@ant-design/icons'
 
 export const CourseGeneralActions = () => {
   return (
     <StyledCourseGeneralActions>
+      <Action>
+        <HeartFilled />
+        <span>Acción 1</span>
+      </Action>
       
+      <Action>
+        <MinusSquareFilled />
+        <span>Acción 1</span>
+      </Action>
+      
+      <Action>
+        <CameraFilled />
+        <span>Acción 1</span>
+      </Action>
     </StyledCourseGeneralActions>
   )
 }
--- src/components/course-general-actions/Styles.js
+++ src/components/course-general-actions/Styles.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
 export const StyledCourseGeneralActions = styled.div`
-  
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+export const Action = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 16px;
+  color: #8d8d9d;
+  font-weight: 700;
+
+  & > span:last-child {
+    margin-top: 8px;
+  }
 `;
