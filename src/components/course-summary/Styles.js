34973eb0efd90918fddbfed802e42d45e943dfc5
stevenacz <scoailazaa@gmail.com>
2021-02-27 18:56:57 -0500

    improved

--- src/components/course-summary/Styles.js
+++ src/components/course-summary/Styles.js
@@ -3,7 +3,29 @@ import styled from 'styled-components';
 export const StyledCourseSummary = styled.section`
   max-width: 1152px;
   margin: 0 auto;
+  padding: 48px 0;
+
+  & > h3 {
+    font-size: 32px;
+    font-weight: 900;
+    line-height: 44px;
+    margin-bottom: 24px;
+  }
+
+  & > p {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    margin-bottom: 24px;
+    max-width: 662px;
+  }
   
   @media (max-width: 768px) {
+    padding: 40px 20px;
+
+    & > h3 {
+      font-size: 24px;
+      line-height: 32px;
+    }
   }
 `;
