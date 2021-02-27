3381b10063d2d33434ecd7e2ecdc18e679b8859d
stevenacz <scoailazaa@gmail.com>
2021-02-26 23:53:59 -0500

    improved

--- src/components/course-hero/CourseHero.js
+++ src/components/course-hero/CourseHero.js
@@ -29,7 +29,7 @@ export const CourseHero = () => {
       <BrowserView>
         <DesktopCourseHero>
           <HeroInformation>
-            <h2>Ttile H2 - Nombre del curso</h2>
+            <h2>Title H2 - Nombre del curso</h2>
             <p>
               Body 3- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id mi, mauris aliquam phasellus quis semper diam fringilla. Nunc nullam est in non. Enim sapien amet ut pharetra. Purus sagittis est felis sapien in. Auctor tellus, in mauris mi, facilisi arcu sedut.
             </p>
--- src/pages/course-detail/Styles.js
+++ src/pages/course-detail/Styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const CourseDetail = styled.main`
   max-width: 1152px;
   height: 100%;
-  margin: 0 auto;
+  margin: 41px auto;
   
   @media (max-width: 1152px) {
   }
