81384d7a89fe5f96bac0c570cf373e67cd001d92
stevenacz <scoailazaa@gmail.com>
2021-02-28 18:01:16 -0500

    Styled added

---
+++ src/components/comment/Styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const StyledComment = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+
+  & > div:first-child {
+    width: 40px;
+    height: 40px;
+    background-color: #e8eaf0;
+    border-radius: 10px;
+    margin-right: 16px;
+  }
+
+  & > div:last-child {
+    display: flex;
+    flex-direction: column;
+
+    & > span {
+      font-size: 16px;
+      font-weight: 700;
+    }
+
+    & > div {
+      color: #8d8d9d;
+    }
+  }
+`;
+
+export const Body = styled.div`
+  & > p {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+  }
+`;
