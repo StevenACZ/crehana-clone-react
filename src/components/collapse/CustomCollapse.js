3ed47ce7f229f77a60bea3a803b70317582d03bb
stevenacz <scoailazaa@gmail.com>
2021-02-28 18:43:43 -0500

    component ready

---
+++ src/components/collapse/CustomCollapse.js
@@ -0,0 +1,21 @@
+import React, { Children } from 'react';
+
+// Styles
+import {
+  StyledCustomCollapse
+} from './Styles';
+
+export const CustomCollapse = ({
+  children,
+  ...rest
+}) => {
+  return (
+    <StyledCustomCollapse
+      accordion
+      expandIconPosition="right"
+      { ...rest }
+    >
+      { children }
+    </StyledCustomCollapse>
+  )
+}
