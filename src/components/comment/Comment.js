20f786f9d5537a92e958fb8b54d702810311e5a3
stevenacz <scoailazaa@gmail.com>
2021-02-28 18:01:06 -0500

    component init

---
+++ src/components/comment/Comment.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// Styles
+import {
+  StyledComment,
+  Header,
+  Body
+} from './Styles';
+
+// Icons ANTD
+import {
+  StarFilled,
+  StarOutlined
+} from '@ant-design/icons';
+
+export const Comment = () => {
+  return (
+    <StyledComment>
+      <Header>
+        <div></div>
+        <div>
+          <span>Usuario213</span>
+          <div>
+            <StarFilled />
+            <StarFilled />
+            <StarFilled />
+            <StarFilled />
+            <StarOutlined />
+          </div>
+        </div>
+      </Header>
+
+      <Body>
+        <p>
+          Body 3- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id mi, mauris aliquam phasellus quis semper diam fringilla. Nunc nullam est in non. Enim sapien amet ut pharetra. Purus sagittis est felis sapien in. Auctor tellus, in mauris mi, facilisi arcu sed ut.
+        </p>
+      </Body>
+    </StyledComment>
+  )
+}
