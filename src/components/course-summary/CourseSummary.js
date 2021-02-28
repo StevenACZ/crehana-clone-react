dd70ee38b5f59ef4fe8e5e3ed016f7c4e26687e8
stevenacz <scoailazaa@gmail.com>
2021-02-28 18:48:22 -0500

    improved

--- src/components/course-summary/CourseSummary.js
+++ src/components/course-summary/CourseSummary.js
@@ -18,7 +18,7 @@ export const CourseSummary = () => {
       <p>
         Body 3- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id mi, mauris aliquam phasellus quis semper diam fringilla. Nunc nullam est in non. Enim sapien amet ut pharetra. Purus sagittis est felis sapien in. Auctor tellus, in mauris mi, facilisi arc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id mi, mauris aliquam phasellus quis semper diam fringilla. Nunc nullam est in non. Enim sapien amet ut pharetra. Purus sagittis est felis sapien in. Auctor tellus, in mauris mi, facilisi arcu sedut.
       </p>
-      <CustomButton anchorLink="true">
+      <CustomButton anchorlink="true">
         textlink <DownOutlined />
       </CustomButton>
     </StyledCourseSummary>
