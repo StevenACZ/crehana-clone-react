747b22504cf2da41057318e5d4a0d09b96dd222a
stevenacz <scoailazaa@gmail.com>
2021-02-28 23:28:24 -0500

    improved

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,25 +1,15 @@
 export const color = {
-  // colorPrimary: '#181b32',
-  colorPrimary: 'red',
-  // colorPrimaryLight: '#7ed56f',
-  // colorPrimaryDark: '#28b485',
+  colorPrimary: '#181b32',
+  // colorPrimary: 'red', // test //
 
-  // colorSecondaryLight: '#ffb900',
-  // colorSecondaryDark: '#ff7730',
-
-  // $color-tertiary-light: #2998ff;
-  // $color-tertiary-dark: #5643fa;
-
-  // colorGreyLight1: '#E8EAF0',
-  colorGreyLight1: 'blue',
-  // colorGreyLight2: '#F6F7F9',
-  colorGreyLight2: 'peru',
-  // colorGreyLight3: '#8D8D9D',
-  colorGreyLight3: 'green',
-
-  // $color-grey-dark: #777;
-  // $color-grey-dark-2: #999;
-  // $color-grey-dark-3: #333;
+  colorGreyLight1: '#E8EAF0',
+  // colorGreyLight1: 'blue', // test //
+  colorGreyLight2: '#F6F7F9',
+  // colorGreyLight2: 'peru', // test //
+  colorGreyLight3: '#8D8D9D',
+  // colorGreyLight3: 'green', // test //
+  colorGreyLight4: '#DFE1E6',
+  // colorGreyLight4: 'yellow', // test //
 
   colorWhite: '#fff',
   colorBlack: '#000'
