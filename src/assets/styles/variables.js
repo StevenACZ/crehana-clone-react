22088b124455a1b54d8c5fb05c0f4e34da9a1baa
stevenacz <scoailazaa@gmail.com>
2021-02-28 22:09:59 -0500

    colors

---
+++ src/assets/styles/variables.js
@@ -0,0 +1,22 @@
+export const color = {
+  colorPrimary: '#181b32',
+  // colorPrimaryLight: '#7ed56f',
+  // colorPrimaryDark: '#28b485',
+
+  // colorSecondaryLight: '#ffb900',
+  // colorSecondaryDark: '#ff7730',
+
+  // $color-tertiary-light: #2998ff;
+  // $color-tertiary-dark: #5643fa;
+
+  colorGreyLight1: '#E8EAF0',
+  colorGreyLight2: '#F6F7F9',
+  colorGreyLight3: '#8D8D9D',
+
+  // $color-grey-dark: #777;
+  // $color-grey-dark-2: #999;
+  // $color-grey-dark-3: #333;
+
+  colorWhite: '#fff',
+  colorBlack: '#000'
+}
