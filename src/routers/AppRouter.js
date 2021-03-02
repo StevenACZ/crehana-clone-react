d1a615d2e13ca712350288106209333ae8959776
stevenacz <scoailazaa@gmail.com>
2021-03-02 17:19:25 -0500

    improved routers

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react';
-import { HashRouter, Switch } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 // Auth
 import { AuthContext } from '../auth/AuthContext';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
 
-import { DashboardRouter } from './DashboardRouter';
+import { CheckoutScreen } from '../pages/checkout/CheckoutScreen';
+import { CourseCatalogScreen } from '../pages/course-catalog/CourseCatalogScreen';
+import { CourseDetailScreen } from '../pages/course-detail/CourseDetailScreen';
 import { LoginScreen } from '../pages/login/LoginScreen';
+import { Header } from '../components/header/Header';
 
 export const AppRouter = () => {
 
@@ -15,19 +18,32 @@ export const AppRouter = () => {
 
   return (
     <HashRouter>
+      <Header />
+      
       <Switch>
+        <Route
+          exact
+          path="/"
+          component={ CourseCatalogScreen }
+        />
+        <Route
+          exact
+          path="/course"
+          component={ CourseDetailScreen }
+        />
         <PublicRoute
           exact
           path="/login"
           component={ LoginScreen }
           isAuthenticated={ user.logged }
         />
-
         <PrivateRoute
-          path="/"
-          component={ DashboardRouter }
+          exact
+          path="/checkout"
+          component={ CheckoutScreen }
           isAuthenticated={ user.logged }
         />
+        <Redirect to="/" />
       </Switch>
     </HashRouter>
   )
