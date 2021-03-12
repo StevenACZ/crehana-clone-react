dd041756eb373d6d08fd613782f09ae62245c6b5
stevenacz <scoailazaa@gmail.com>
2021-03-12 17:55:55 -0500

    improved

--- src/customHooks/useForm.js
+++ src/customHooks/useForm.js
@@ -1,13 +1,14 @@
+// React
 import { useState } from 'react'
 
-const useForm = (initialState = {}) => {
-  const [values, setValues] = useState( initialState )
+const useForm = ( initialState = {} ) => {
+  const [ values, setValues ] = useState( initialState )
 
   const reset = () => {
     setValues( initialState );
   }
 
-  const handleInputChange = ({ target }) => {
+  const handleInputChange = ( { target } ) => {
     setValues({
       ...values,
       [ target.name ]: target.value
