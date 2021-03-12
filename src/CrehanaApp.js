955453415fa150b69aeb1d1dd023d42cc6290e84
stevenacz <scoailazaa@gmail.com>
2021-03-12 18:43:25 -0500

    context to redux

--- src/CrehanaApp.js
+++ src/CrehanaApp.js
@@ -1,38 +1,21 @@
 // React
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 
-// React - Router
-import { AppRouter } from './routers/AppRouter';
-
-// Auth
-import { AuthContext } from './auth/AuthContext';
-import { authReducer } from './auth/authReducer';
+// Redux
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 // Global Styles
 import 'antd/dist/antd.css';
 import './assets/styles/App.css';
 
-// Basic Login
-const init = () => {
-  return JSON.parse(localStorage.getItem('user')) || { logged: false };
-}
+// Components
+import { AppRouter } from './routers/AppRouter';
 
 export const CrehanaApp = () => {
-
-  const [ user, dispatch ] = useReducer( authReducer, {}, init );
-
-  useEffect(() => {
-    localStorage.setItem( 'user', JSON.stringify( user ) );
-  }, [ user ])
-
   return (
-    <AuthContext.Provider
-      value={{
-        user,
-        dispatch
-      }}
-    >
+    <Provider store={ store }>
       <AppRouter />
-    </AuthContext.Provider>
+    </Provider>
   )
 }
