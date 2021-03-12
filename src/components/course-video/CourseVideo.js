8a24b290ca84901d2b4035ba23e19cef10ebf731
stevenacz <scoailazaa@gmail.com>
2021-03-12 17:51:28 -0500

    improved

--- src/components/course-video/CourseVideo.js
+++ src/components/course-video/CourseVideo.js
@@ -1,14 +1,11 @@
+// React
 import React from 'react';
 
 // Styles
-import {
-  StyledCourseVideo
-} from './Styles';
+import { StyledCourseVideo } from './Styles';
 
 // Icons ANTD
-import {
-  PlayCircleFilled
-} from '@ant-design/icons';
+import { PlayCircleFilled } from '@ant-design/icons';
 
 export const CourseVideo = () => {
   return (
