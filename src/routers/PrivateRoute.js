144f003682f85b1aaa9790be6f968805ab5f2c6b
stevenacz <scoailazaa@gmail.com>
2021-03-12 17:40:21 -0500

    improved

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,6 +1,10 @@
+// React
 import React from 'react';
+
+// Router Router
 import { Redirect, Route } from 'react-router-dom';
 
+// Prop Types
 import PropTypes from 'prop-types';
 
 export const PrivateRoute = ({
